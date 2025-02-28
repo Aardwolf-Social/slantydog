33cb798110ebb7dd45d2e953b0e0ffbd4b255b57
Banjo Fox <drbanjofox@protonmail.com>
2025-02-28 16:40:19 -0500

    Add a starter tests/ directoy*

---
+++ tests/models.rs
@@ -0,0 +1,15 @@
+// tests/models.rs
+#[cfg(test)]
+mod tests {
+    use super::models;
+
+    #[test]
+    fn test_post_data() {
+        // Test the PostData struct here...
+    }
+
+    #[test]
+    fn test_post() {
+        // Test the Post struct here...
+    }
+}
