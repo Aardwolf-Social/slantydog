c53005a43cd763d6a06068af90ab7ffd0f48cfbb
Banjo Fox <drbanjofox@protonmail.com>
2025-03-13 11:58:48 -0400

    Moving the database engines up a directory, and removing the directory containing them.  Trying to resolve import issues.

--- aardwolf-api/database-api/src/database_engines/lib.rs
+++
@@ -1,8 +0,0 @@
-// database-api/src/database_engines/lib.rs
-pub mod mysql_files;
-pub mod postgres_files;
-pub mod sqlite_files;
-
-pub use mysql_files;
-pub use postgres_files;
-pub use sqlite_files;
--- aardwolf-api/database-api/src/lib.rs
+++ aardwolf-api/database-api/src/lib.rs
@@ -1,3 +1,6 @@
 // database-api/src/lib.rs
 pub mod connection_pool;
 pub mod database_main;
+pub mod mysql;
+pub mod postgres;
+pub mod sqlite;
