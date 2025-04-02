d2e79dcb33a22bdf00483ea314d4819bd33d126b
Banjo Fox <drbanjofox@protonmail.com>
2025-04-02 13:23:21 -0400

    Splitting more frontend files to be backend agnostic.

--- aardwolf-api/frontend-api/src/api_user_routes.rs
+++ aardwolf-api/frontend-api/src/api_user_routes.rs
@@ -1,34 +1,52 @@
 // aardwolf-api/frontend-api/src/api_user_routes.rs
 
-use crate::routes;
-use crate::handlers;
+pub trait ApiUserRoutes {
+    fn get_users_route(&self) -> Box<dyn RouteHandler>;
+    fn get_user_posts_route(&self) -> Box<dyn RouteHandler>;
+    fn get_user_followers_route(&self) -> Box<dyn RouteHandler>;
+    fn follow_user_route(&self) -> Box<dyn RouteHandler>;
+    fn unfollow_user_route(&self) -> Box<dyn RouteHandler>;
+}
+
+pub trait RouteHandler {
+    fn handle(&self, request: &Request) -> Response;
+}
+
+// Define a struct to hold the routes
+pub struct ApiUserRoutesImpl {
+    pub get_users: Box<dyn RouteHandler>,
+    pub get_user_posts: Box<dyn RouteHandler>,
+    pub get_user_followers: Box<dyn RouteHandler>,
+    pub follow_user: Box<dyn RouteHandler>,
+    pub unfollow_user: Box<dyn RouteHandler>,
+}
+
+impl ApiUserRoutes for ApiUserRoutesImpl {
+    fn get_users_route(&self) -> Box<dyn RouteHandler> {
+        self.get_users.clone()
+    }
+
+    fn get_user_posts_route(&self) -> Box<dyn RouteHandler> {
+        self.get_user_posts.clone()
+    }
+
+    fn get_user_followers_route(&self) -> Box<dyn RouteHandler> {
+        self.get_user_followers.clone()
+    }
+
+    fn follow_user_route(&self) -> Box<dyn RouteHandler> {
+        self.follow_user.clone()
+    }
+
+    fn unfollow_user_route(&self) -> Box<dyn RouteHandler> {
+        self.unfollow_user.clone()
+    }
+}
 
 // Define the main function to create the HTTP server
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(web::resource("/api/users").route(web::get().to(handlers::get_users)))
-            .configure(|cfg| {
-                cfg.service(
-                    web::resource("/api/users/:userId/posts")
-                        .route(web::get().to(handlers::get_user_posts)),
-                )
-                .service(
-                    web::resource("/api/users/:userId/followers")
-                        .route(web::get().to(handlers::get_user_followers)),
-                )
-                .service(
-                    web::resource("/api/users/follow/:userId")
-                        .route(web::get().to(handlers::follow_user)),
-                )
-                .service(
-                    web::resource("/api/users/unfollow/:userId")
-                        .route(web::get().to(handlers::unfollow_user)),
-                );
-            })
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
-}
+    let routes = ApiUserRoutesImpl {
+        get_users: Box::new(handlers::GetUsersHandler),
+        get_user_posts: Box::new(handlers::GetUserPostsHandler),
+        get_user_followers: Box
